60fd5c0e57ac586c8862f0c71444574ff42c98df
Ancalima Silme <ancalimasilme@gmail.com>
2015-05-05 21:42:39 +0600

    initial commit

---
+++ init.rb
@@ -0,0 +1,8 @@
+Redmine::Plugin.register :email_grabber do
+  name 'Email Grabber plugin'
+  author 'Author name'
+  description 'This is a plugin for Redmine'
+  version '0.0.1'
+  url 'http://example.com/path/to/plugin'
+  author_url 'http://example.com/about'
+end
