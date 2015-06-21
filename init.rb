64384ac29b33524547fd74751ea75a3d727741bc
Ancalima Silme <ancalimasilme@gmail.com>
2015-06-21 22:15:50 +0600

    edit init.rb

--- init.rb
+++ init.rb
@@ -2,11 +2,10 @@
 
 Redmine::Plugin.register :email_grabber do
   name 'Email Grabber plugin'
-  author 'Author name'
-  description 'This is a plugin for Redmine'
+  author 'Alexander Kazachkin'
+  description 'This is a plugin for Redmine. It is help you get email messages and create issues'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/AncalimaSilme/email_grabber'
 
   menu :admin_menu, :email, { :controller => 'emails', :action => 'index' }, :caption => :emails_page_title
   menu :admin_menu, :conformity_rules, { :controller => 'conformity_rules', :action => 'index' }, :caption => :conformity_rules_page_title
