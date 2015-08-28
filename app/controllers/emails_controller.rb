cf8dabef69bc1622e57837e1bd9afd855668c426
Kazachkin 'Ancalima Silme' Alexander <ancalimasilme@gmail.com>
2015-08-28 16:15:19 +0600

    pagination for email messages list

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -2,7 +2,13 @@ class EmailsController < ApplicationController
   layout 'admin'
 
   def index
+    @limit = per_page_option
+    
     @emails = Email.order("id DESC")
+    @email_count = @emails.size
+    @email_pages = Paginator.new @email_count, @limit, params['page']
+    @offset ||= @email_pages.offset
+    @emailss = @emails.limit(@limit).offset(@offset)
   end
 
   def show
